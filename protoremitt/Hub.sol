f9ab4b15d1a19a17e7f4ef67159f666ac648f294
0xvoidfarmer <30801219+0xvoidfarmer@users.noreply.github.com>
2017-08-23 12:40:05 +0100

    Hub
    Basic Solidity contract hub template

---
+++ protoremitt/Hub.sol
@@ -0,0 +1,72 @@
+
+pragma solidity ^0.4.6;
+
+import  "./Spawn.sol" ;
+import  "./Freezeable.sol";
+
+contract Hub is Freezeable {
+     
+     address[] public spawns;
+     mapping (address => bool) spawnExists;
+     
+     modifier onlyIfSpawn (address spawnAddress){
+         if(spawnExists[spawnAddress] == false) throw;
+         _;
+     }
+     
+     event LogNewSpawn(address spwanAddress /*, [type1 arg1, ...typen, argn] spawn specific parameters */);
+     
+     function getSpawnCount()
+        public 
+        constant
+        returns (uint spawnCount)
+    {
+        return spawns.length;
+    }
+    
+    function newSpawn(/*[type1 arg1, ...typen, argn] spawn specific parameters*/)
+        public 
+        returns (address spawnContract)
+    {
+        /*
+        Spawn trustedSpawn = new Spawn(msg.sender, [type1 arg1, ...typen, argn]  );
+        spawns.puhs(trustedSpawn);
+        spawnExists[trustedSpawn] = true;
+        LogNewSpawn(trustedSpawn,[type1 arg1, ...typen, argn] );
+        return trustedSpawn;
+        */
+    }
+    
+    //Pass through Admin Controls
+    
+    function freezeSpawn(address _spawnAddress)
+        onlyOwner
+        onlyIfSpawn (_spawnAddress)
+        returns (bool success)
+    {
+        Spawn trustedSpawn = Spawn(_spawnAddress);
+        return (trustedSpawn.switchRunningState(true));
+    }
+    
+    function unfreezeSpawn(address _spawnAddress)
+        onlyOwner
+        onlyIfSpawn(_spawnAddress)
+        returns (bool success)
+    {
+        Spawn trustedSpawn = Spawn(_spawnAddress);
+        return (trustedSpawn.switchRunningState(false));
+    }
+    
+    function changeSpawnOwner(address _spawnAddress, address newOwner)
+        onlyOwner
+        onlyIfSpawn(_spawnAddress)
+        returns (bool success)
+    {
+        Spawn trustedSpawn = Spawn(_spawnAddress);
+        return (trustedSpawn.changeOwner(newOwner));
+    }
+        
+        
+     
+     
+}
