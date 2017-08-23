6e45264f4e646ab3ae0c3b4c1a404974fb81a051
0xvoidfarmer <30801219+0xvoidfarmer@users.noreply.github.com>
2017-08-23 11:25:19 +0100

    Create Owned.sol

---
+++ protoremitt/Owned.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.4.12;
+
+contract Owned {
+    address owner;
+    
+    modifier onlyOwner { if(msg.sender != owner) throw;  _;}
+    event LogNewOwner(address oldOwner, address newOwner);
+    
+    
+    function Owned() {
+        owner = msg.sender;
+    }
+    
+    function  changeOwner(address newOwner) 
+        onlyOwner
+        returns (bool success)
+    {
+        if (newOwner == address(0)) throw;
+        LogNewOwner(owner, newOwner);
+        owner = newOwner;
+        return true;
+    }
+}
