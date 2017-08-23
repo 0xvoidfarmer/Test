c0d8b07dc76471478224f5d1f1458f448828cb10
0xvoidfarmer <30801219+0xvoidfarmer@users.noreply.github.com>
2017-08-23 11:55:29 +0100

    Create Freezeable.sol

---
+++ protoremitt/Freezeable.sol
@@ -0,0 +1,32 @@
+
+import "Owned.sol";
+
+contract Freezeable is Owned {
+    
+    bool public running;
+    
+    event LogOnOffSwitch(bool switchState);
+    
+    modifier onlyIfRunning {
+        if(running == false) throw;
+        _;    
+    }
+    
+    
+    function Freezeable() {
+        running = true;
+    }
+    
+    function switchRunningState(bool onOff)
+        onlyOwner
+        returns (bool success)
+    {
+        running = onOff;
+        LogOnOffSwitch(onOff);
+        return true;
+    }
+    
+    function () {}
+    
+    
+}
